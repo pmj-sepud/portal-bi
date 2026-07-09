fe903283657102264c0af7f5080b106e639ecdc6
joaomatheus-commits <smartmobilityjlle@gmail.com>
2026-07-09 13:19:12 -0300

    Atualizacao automatica - Acidentes Bombeiros UMO - 09/07/2026 13:19

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -100,7 +100,7 @@ window.PORTAL_META = {
   versao: "2.1.0",
   publicacao: "GitHub Pages",
   url: "https://pmj-sepud.github.io/portal-bi/",
-  ultimaAtualizacao: "2026-07-09T13:06",
+  ultimaAtualizacao: "2026-07-09T13:19",
   auditoria: "100% aprovada",
   framework: 1,
   designSystem: 1,

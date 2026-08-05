902c61ffc12a34191ea4868e42c5f80ddfc6a0da
Joao Matheus Santos Miranda <smartmobilityjlle@gmail.com>
2026-08-05 10:07:52 -0300

    Atualizacao automatica - Transporte Público UMO - 05/08/2026 10:07

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -110,7 +110,7 @@ window.PORTAL_META = {
   versao: "2.1.0",
   publicacao: "GitHub Pages",
   url: "https://pmj-sepud.github.io/portal-bi/",
-  ultimaAtualizacao: "2026-08-05T10:04",
+  ultimaAtualizacao: "2026-08-05T10:07",
   auditoria: "100% aprovada",
   framework: 1,
   designSystem: 1,

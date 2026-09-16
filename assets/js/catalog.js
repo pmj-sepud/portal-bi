6b467a3d288a868cb6c0f60f37d589eadad3cb23
Edson Pereira <u40525@pmjlle.joinville.sc.gov.br>
2026-09-16 14:47:17 -0300

    Atualizacao automatica - Portal BI (todos os dashboards) - 16/09/2026 14:47

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -107,7 +107,7 @@ window.PORTAL_META = {
   versao: "2.1.0",
   publicacao: "GitHub Pages",
   url: "https://pmj-sepud.github.io/portal-bi/",
-  ultimaAtualizacao: "2026-09-16T14:38",
+  ultimaAtualizacao: "2026-09-16T14:47",
   auditoria: "100% aprovada",
   framework: 1,
   designSystem: 1,

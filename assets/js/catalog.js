36938c699d08b4c8daf8ca0a245b6db01a8d923c
Edson Pereira <u40525@pmjlle.joinville.sc.gov.br>
2026-09-17 11:44:52 -0300

    Atualizacao automatica - Equipamentos SEPUR - 17/09/2026 11:44

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -28,7 +28,7 @@ window.PORTAL_CATALOG = {
       grupo: "Tecnologia", versao: "v2.0", responsavel: "Secretaria de Pesquisa e Planejamento Urbano – SEPUR",
       fonte: "Controle Patrimonial de CPUs", tags: ["patrimônio", "TI", "equipamentos"],
       descricao: "Controle patrimonial de CPUs, kits e equipamentos de informática distribuídos pela Secretaria de Pesquisa e Planejamento Urbano – SEPUR.",
-      href: "dashboards/equipamentos/", bases: 1, atualizacao: "2026-09-16",
+      href: "dashboards/equipamentos/", bases: 1, atualizacao: "2026-09-17",
       keywords: ["equipamentos", "cpu", "patrimonio", "sepur", "informatica"],
       icone: '<rect x="3" y="4" width="18" height="12" rx="2"/><path d="M8 20h8M12 16v4"/>',
       paineis: [{ nome: "Equipamentos SEPUR", registros: 77, atualizacao: "2026-09-09" }]
@@ -107,7 +107,7 @@ window.PORTAL_META = {
   versao: "2.1.0",
   publicacao: "GitHub Pages",
   url: "https://pmj-sepud.github.io/portal-bi/",
-  ultimaAtualizacao: "2026-09-17T11:33",
+  ultimaAtualizacao: "2026-09-17T11:44",
   auditoria: "100% aprovada",
   framework: 1,
   designSystem: 1,

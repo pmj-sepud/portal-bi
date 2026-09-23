d6fbfdc48425072f6308834849c301d2069b46b4
Edson Pereira <u40525@pmjlle.joinville.sc.gov.br>
2026-09-23 10:02:27 -0300

    Atualizacao automatica - Waze UMO - 23/09/2026 10:02

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -107,7 +107,7 @@ window.PORTAL_META = {
   versao: "2.1.0",
   publicacao: "GitHub Pages",
   url: "https://pmj-sepud.github.io/portal-bi/",
-  ultimaAtualizacao: "2026-09-23T09:48",
+  ultimaAtualizacao: "2026-09-23T10:02",
   auditoria: "100% aprovada",
   framework: 1,
   designSystem: 1,
